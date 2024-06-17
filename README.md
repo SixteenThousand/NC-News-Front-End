2eb9c874632af787269abbb39b4d5f0e7ef7cea9
Thomas Lindop <thomas16256@gmail.com>
2024-06-17 10:24:21 +0100

    >> initialise app skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# NC News: The Front End
+
+Front End for ![NC-News](https://github.com/SixteenThousand/NC-News).
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return <>
+    <p>Hi! I'm  a react app.</p>
+  </>;
+}
+
+export default App
