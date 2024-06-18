9170c84a02508783e070069c9279832b0418f454
Thomas Lindop <thomas16256@gmail.com>
2024-06-18 15:16:43 +0100

    >> link header title to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import Login from "./Login";
+import { Link, } from "react-router-dom";
 
 export default function Header({}) {
   return <header className="header">
-    <h1>NCN</h1>
-    <h2>All the code, all the news</h2>
+    <Link to="/">
+      <div className="title">
+        <h1>NCN</h1>
+        <h2>All the code, all the news</h2>
+      </div>
+    </Link>
     <Login />
   </header>;
 }
