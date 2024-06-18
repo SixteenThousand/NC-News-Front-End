05b273555601823b6be6198b3289130a7342a655
Thomas Lindop <thomas16256@gmail.com>
2024-06-18 15:52:03 +0100

    >> fix post date bug on home page > format post date nicely

--- src/components/ArticleSummary.jsx
+++ src/components/ArticleSummary.jsx
@@ -2,6 +2,14 @@ import { Link } from "react-router-dom";
 
 
 export default function ArticleSummary({ data, }) {
+  const postedDate = new Date(data.created_at);
+  const postedDateStr = "".concat(
+    postedDate.getFullYear(),
+    "-",
+    (postedDate.getMonth()+1).toString().padStart(2,'0'),
+    "-",
+    postedDate.getDate());
+  
   return <Link to={`/articles/${data.article_id}`}>
       <article className="article-summary">
       <div className="title">{data.title}</div>
@@ -9,7 +17,7 @@ export default function ArticleSummary({ data, }) {
         <div className="topic">{data.topic}</div>
         <div className="votes">{data.votes}</div>
         <div className="author">By {data.author}</div>
-        <div className="created_at">Posted: {data.votes}</div>
+        <div className="created_at">Posted: {postedDateStr}</div>
       </div>
       <img src={data.article_img_url} alt={`image for ${data.title}`} />
     </article>;
