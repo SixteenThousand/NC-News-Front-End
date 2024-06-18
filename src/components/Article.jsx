0042ae8da42460819cc684628933d839b78f52b3
Thomas Lindop <thomas16256@gmail.com>
2024-06-18 17:28:28 +0100

    >> show article data on the relevant page > refactor date formattting in ArticleSummary into a utils function > bugfix: export Todo properly

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,34 @@
 import { useParams, } from "react-router-dom";
-import { useEffect, } from "react";
+import { useEffect, useState, } from "react";
+import { getArticle } from "../api";
+import Todo from "./Todo";
+import { formatDate, } from "../utils";
 
 
 export default function Article({}) {
   const { article_id } = useParams();
+  const [articleData, setArticleData] = useState(null);
   
   useEffect(() => {
-
+    getArticle(article_id).then((data) => {
+      setArticleData(data);
+    });
   }, []);
   
-  return <p>Helloo! This is article {article_id}</p>;
+  return <article className="article">
+    {function() {
+      if(articleData === null) {
+        return <Todo msg="Error component not built!" />;
+      }
+      return <>
+        <h3>{articleData.title}</h3>
+        <p>By {articleData.author}</p>
+        <p>Posted: {formatDate(articleData.created_at)}</p>
+        <img src={articleData.article_img_url} alt={`image for ${articleData.title}`} />
+        <p>{articleData.body}</p>
+        <h4>comments</h4>
+        <Todo msg="comment endpoint not complete!" />
+      </>;
+    }()}
+  </article>;
 }
--- src/components/ArticleSummary.jsx
+++ src/components/ArticleSummary.jsx
@@ -1,25 +1,18 @@
 import { Link } from "react-router-dom";
+import { formatDate, } from "../utils";
 
 
 export default function ArticleSummary({ data, }) {
-  const postedDate = new Date(data.created_at);
-  const postedDateStr = "".concat(
-    postedDate.getFullYear(),
-    "-",
-    (postedDate.getMonth()+1).toString().padStart(2,'0'),
-    "-",
-    postedDate.getDate());
-  
-  return <Link to={`/articles/${data.article_id}`}>
-      <article className="article-summary">
+  return <Link className="link" to={`/articles/${data.article_id}`}>
+    <article className="article-summary">
       <div className="title">{data.title}</div>
       <div className="details">
         <div className="topic">{data.topic}</div>
         <div className="votes">{data.votes}</div>
         <div className="author">By {data.author}</div>
-        <div className="created_at">Posted: {postedDateStr}</div>
+        <div className="created_at">Posted: {formatDate(data.created_at)}</div>
       </div>
       <img src={data.article_img_url} alt={`image for ${data.title}`} />
-    </article>;
+    </article>
   </Link>;
 }
