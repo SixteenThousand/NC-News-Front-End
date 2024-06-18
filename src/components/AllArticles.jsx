8dd16df520dee2386e23e4b2e1428a1f5d7e2456
Thomas Lindop <thomas16256@gmail.com>
2024-06-18 11:10:12 +0100

    >> refactor requestUrl state to be requestConf, an axios config object >> make home page show more article data

--- src/components/AllArticles.jsx
+++ src/components/AllArticles.jsx
@@ -4,11 +4,12 @@ import Paginator from "./Paginator";
 
 
 export default function AllArticles({}) {
-  const [requestUrl, setRequestUrl] = useState("http://localhost:8000/api/articles");
+  // the axios config needed to request the articles
+  const [requestConf, setRequestConf] = useState({ url: "/articles"});
   return <div className="all-articles">
-{/*     <SearchBar setRequestUrl={setRequestUrl} /> */}
+{/*     <SearchBar setRequestConf={setRequestConf} /> */}
     <Paginator
-      requestUrl={requestUrl}
+      requestConf={requestConf}
       ItemComponent={ArticleSummary}
       idKey="article_id"
       itemsKey="articles"
@@ -20,6 +21,11 @@ export default function AllArticles({}) {
 // export default
 function ArticleSummary({ data, }) {
   return <article className="article-summary">
-    <div>{data.title}</div>
+    <div className="title">{data.title}</div>
+    <div className="topic">{data.topic}</div>
+    <div className="votes">{data.votes}</div>
+    <div className="author">By {data.author}</div>
+    <div className="created_at">Posted: {data.votes}</div>
+    <img src={data.article_img_url} alt={`image for ${data.title}`} />
   </article>;
 }
--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect, } from "react";
-import axios from "axios";
+import api from "../api.js";
 
 
 /**
 * Renders a list of items fetched from a given URL, paginating as appropriate
 * (pagination not built yet!)
-* @prop requestUrl: string - the url from which requests for the item data
-* is made
+* @prop requestConf: Object - the axios config needed to request the items'
+* data
 * @prop ItemComponent: React Component - the component to be used when
 * rendering the data of each item.
 * @prop idKey: String - a key that every item has which uniquely identifies
@@ -14,14 +14,16 @@ import axios from "axios";
 * @prop itemsKey: String - the key on the response data where the list of
 * items is stored
 */
-export default function Paginator({ requestUrl, ItemComponent, idKey, itemsKey }) {
+export default function Paginator({ requestConf, ItemComponent, idKey, itemsKey }) {
   const [items, setItems] = useState([]);
   
   useEffect(() => {
-    axios.get(requestUrl)
-      .then(({ data }) => {
-        setItems(data[itemsKey]);
-      });
+    // for later
+    // requestConf.params.p = //some page state
+    // requestConf.params.limit = 10;
+    api.request(requestConf).then(({ data }) => {
+      setItems(data[itemsKey]);
+    });
   }, []);
 
   return <ol className="paginator">
