d7ef76bd612b4627e51fe6c5ba05a36323d4adb3
Thomas Lindop <thomas16256@gmail.com>
2024-06-18 11:59:43 +0100

    >> refactor ArticleSummary into its own module

--- src/components/AllArticles.jsx
+++ src/components/AllArticles.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import ArticleSummary from "./ArticleSummary";
+import ArticleSummary from "./ArticleSummary";
 import Paginator from "./Paginator";
 
 
@@ -15,19 +15,4 @@ export default function AllArticles({}) {
       itemsKey="articles"
     />
   </div>;
-}
-
-
-// export default
-function ArticleSummary({ data, }) {
-  return <article className="article-summary">
-    <div className="title">{data.title}</div>
-    <div className="details">
-      <div className="topic">{data.topic}</div>
-      <div className="votes">{data.votes}</div>
-      <div className="author">By {data.author}</div>
-      <div className="created_at">Posted: {data.votes}</div>
-    </div>
-    <img src={data.article_img_url} alt={`image for ${data.title}`} />
-  </article>;
 }
