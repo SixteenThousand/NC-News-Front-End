228ff90c3f7713df97af4d6e0ee2e6cc2238989e
Thomas Lindop <thomas16256@gmail.com>
2024-06-18 13:56:49 +0100

    >> make app use hosted API instead of localhost

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://nc-news-mwvr.onrender.com/api",
+  // baseURL: "http://localhost:8000/api",
 });
