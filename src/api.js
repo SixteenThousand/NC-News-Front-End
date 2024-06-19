420336043afb51a4336a45477e498b075c970a0d
Thomas Lindop <thomas16256@gmail.com>
2024-06-19 16:24:25 +0100

    >> add api handling functions

--- src/api.js
+++ src/api.js
@@ -13,3 +13,31 @@ export function getArticle(articleId) {
       return data;
     });
 }
+
+export function getComments(articleId) {
+  return axiosInst.get(`/articles/${articleId}/comments`)
+    .then(({ data }) => {
+      return data;
+    });
+}
+
+export function getAllArticles() {
+  return axiosInst.get("/articles")
+    .then(({ data }) => {
+      return data;
+    });
+}
+
+export function voteOnComment(commentId) {
+  return axiosInst.patch(`/comments/${commentId}`)
+    .then(({ data }) => {
+      return data.votes;
+    });
+}
+
+export function voteOnArticle(articleId) {
+  return axiosInst.patch(`/articles/${articleId}`)
+    .then(({ data }) => {
+      return data.votes;
+    });
+}
