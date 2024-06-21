2fbdc26fd0c023829798103c2d319b8ae3822e64
Thomas Lindop <thomas16256@gmail.com>
2024-06-21 14:11:48 +0100

    >> finish testing formatDate >> add compareByKey utils function > fully tested

--- src/tests.test.js
+++ src/tests.test.js
@@ -8,11 +8,63 @@ const imports = Promise.all([
 
 imports.then(([t, utils, assert]) => {
   t.describe("formatDate", () => {
+    const examples = [
+      "2000-02-15T14:00:00.890Z",
+      "2013-12-30T08:02:10.890Z",
+      "2000-02-15T21:09:43.890Z",
+      "1993-10-07T20:47:47.890Z",
+      "1993-10-07T20:47:47.890Z",
+    ];
     t.test("returns a string in the correct format", () => {
-      const date = 1234567890;
-      const result = utils.formatDate(date);
-      assert.equal(typeof(result),"string");
-      assert.match(result, /\d{4}-\d{2}-\d{2}/);
+      let result;
+      examples.forEach((dateStr) => {
+        result = utils.formatDate(Number(new Date(dateStr)));
+        assert.equal(typeof(result),"string");
+        assert.match(result, /\d{4}-\d{2}-\d{2}/);
+      });
+    });
+    t.test("returns the correct date", () => {
+      let result;
+      examples.forEach((dateStr) => {
+        result = utils.formatDate(Number(new Date(dateStr)));
+        assert.equal(result,dateStr.slice(0,10));
+      });
+    });
+    t.test("when passed a second argument equal to true, also adds the time" +
+      "in YYYY-MM-DD HH:MM format",
+      () => {
+        let result;
+        examples.forEach((dateStr) => {
+          result = utils.formatDate(Number(new Date(dateStr)), true);
+          assert.match(result,/\d{4}-\d{2}-\d{2} \d{2}:\d{2}/);
+        });
+      }
+    );
+  });
+  
+  t.describe("utils.compareByKey", () => {
+    const a = {
+      name: "Steve",
+      species: "Salmon",
+      age: 37,
+      fav_ocean: "atlantic",
+    };
+    const b = {
+      name: "Tanisha",
+      age: 27,
+      fav_ocean: "atlantic",
+    };
+    t.test("is pure", () => {
+      const aCopy = structuredClone(a);
+      const bCopy = structuredClone(b);
+      utils.compareByKey("name")(a,b);
+      assert.deepEqual(a, aCopy);
+      assert.deepEqual(b, bCopy);
+    });
+    t.test("compares two objects by the specified key", () => {
+      assert.equal(Math.sign(utils.compareByKey("name")(a,b)), -1);
+      assert.equal(Math.sign(utils.compareByKey("age")(a,b)), 1);
+      assert.equal(Math.sign(utils.compareByKey("fav_ocean")(a,b)), 0);
     });
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,40 @@
 /**
- * Converts
+ * Converts a time stamp representing time since Epoch to a string
+ * representation of it in the current locale.
+ *   @param timeSinceEpoch: Number - the time stamp
+ *   @param includeTime: Boolean - whether to include the time as well as
+ *   the date
 */
-export function formatDate(timeSinceEpoch) {
+export function formatDate(timeSinceEpoch,includeTime) {
+  function format2Digits(num) {
+    return num.toString().padStart(2,'0');
+  }
   const postedDate = new Date(timeSinceEpoch);
-  return "".concat(
+  let result = "".concat(
     postedDate.getFullYear(),
     "-",
-    (postedDate.getMonth()+1).toString().padStart(2,'0'),
+    format2Digits(postedDate.getMonth()+1),
     "-",
-    postedDate.getDate());
+    format2Digits(postedDate.getDate()));
+  if(includeTime) {
+    result = result.concat(
+      " ",
+      format2Digits(postedDate.getHours()),
+      ":",
+      format2Digits(postedDate.getMinutes()));
+  }
+  return result;
+}
+
+/**
+ * Makes comparator functions for objects that compares them by a given key.
+ * For use in .sort or .toSorted
+ * @param key: String - the key to sort by
+ */
+export function compareByKey(key) {
+  return function(a,b) {
+    if(a[key] > b[key]) return 1;
+    if(a[key] === b[key]) return 0;
+    return -1;
+  };
 }
